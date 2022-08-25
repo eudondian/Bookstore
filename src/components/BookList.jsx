2649176f95d0b7864d68cd0ce4cd49a829e33e7e
eudondian <eudondian@gmaile.com>
2022-08-25 10:00:22 +0100

    Modifiy BookList.jsx file

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import { deletedBooks } from '../redux/books/books';
 
-const BookList = ({ title, author }) => (
-  <ul>
-    <h2>{title}</h2>
-    <p>{author}</p>
-    <button type="button">Remove books</button>
-  </ul>
-);
+const BookList = ({ id, title, author }) => {
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(deletedBooks(Number(id)));
+  };
+
+  return (
+    <ul>
+      <div>{title}</div>
+      <div>{author}</div>
+      <button type="button" id={id} onClick={(e) => { handleDelete(e.target.id); }}>Remove books</button>
+    </ul>
+  );
+};
 
 BookList.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
