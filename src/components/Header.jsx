71440ff111e6fe3bfe45a2eee497474da8c8db8e
eudondian <eudondian@gmaile.com>
2022-08-27 19:02:54 +0100

    Modify Header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Avatar } from '@mui/material';
 
 const Header = () => (
   <nav>
     <h1>Bookstore CMS</h1>
-    <ul>
-      <li><Link to="/">Book</Link></li>
-      <li><Link to="/categories">Categories</Link></li>
+    <ul className="nav-links">
+      <li><Link className="links" to="/">Book</Link></li>
+      <li><Link className="links" to="/categories">Categories</Link></li>
     </ul>
+    <Avatar />
   </nav>
 );
 
