2fc0a0fa78297928441e76ca3caea3d821fb4742
eudondian <eudondian@gmaile.com>
2022-08-27 01:15:59 +0100

    Add the delete functunality

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,50 +1,30 @@
-import React from 'react';
 import { useDispatch } from 'react-redux';
-import { PropTypes } from 'prop-types';
-import { deletedBooks } from '../redux/books/books';
+import { deleteBookFromApi } from '../redux/books/books';
 
-const BookList = ({
-  id, title, author, progress, chapter, genre,
-}) => {
+/* eslint-disable react/prop-types */
+function BookList({
+  id, title, author, category,
+}) {
   const dispatch = useDispatch();
-  const handleDelete = (id) => {
-    dispatch(deletedBooks(Number(id)));
+  const chapter = Math.floor(Math.random() * 10);
+
+  const deleteBook = () => {
+    dispatch(deleteBookFromApi(id));
   };
 
   return (
-    <ul>
-      <div>
-        Title:
-        {title}
-      </div>
-      <div>
-        Author:
-        {author}
-      </div>
-      <div>
-        Progress:
-        {progress}
-        %
-      </div>
-      <div>
-        Chapter:
-        {chapter}
-      </div>
-      <div>
-        Genre:
-        {genre}
-      </div>
-      <button type="button" id={id} onClick={(e) => { handleDelete(e.target.id); }}>Remove books</button>
-    </ul>
-  );
-};
+    <div>
+      <h4>{title}</h4>
+      <p>{author}</p>
+      <p>{category}</p>
 
-BookList.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  progress: PropTypes.string.isRequired,
-  chapter: PropTypes.string.isRequired,
-  genre: PropTypes.string.isRequired,
-};
+      <p>
+        <span>Chapter </span>
+        {chapter}
+      </p>
+      <button type="button" onClick={deleteBook}>Remove</button>
+    </div>
+  );
+}
+
 export default BookList;
