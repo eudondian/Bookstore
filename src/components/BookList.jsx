e77c165b44b6f03f186065fcb150d83f419239a8
eudondian <eudondian@gmaile.com>
2022-08-27 18:58:05 +0100

    Modify BookList.jsx file

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,15 @@
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { deleteBookFromApi } from '../redux/books/books';
+import '../styles/index.css';
 
 /* eslint-disable react/prop-types */
 function BookList({
   id, title, author, category,
 }) {
   const dispatch = useDispatch();
+  const progress = 54;
   const chapter = Math.floor(Math.random() * 10);
 
   const deleteBook = () => {
@@ -13,16 +17,46 @@ function BookList({
   };
 
   return (
-    <div>
-      <h4>{title}</h4>
-      <p>{author}</p>
-      <p>{category}</p>
-
-      <p>
-        <span>Chapter </span>
-        {chapter}
-      </p>
-      <button type="button" onClick={deleteBook}>Remove</button>
+    <div className="dashboard-container">
+      <div className="dashboard">
+        <div className="col1">
+          <div className="col1-row1">
+            <p className="category">{category}</p>
+            <h3 className="title">{title}</h3>
+            <p className="author">{author}</p>
+          </div>
+          <div className="col1-row2">
+            <span>Comments</span>
+            <div className="span-dash" />
+            <div>
+              <button type="button" onClick={deleteBook} className="remove-btn">
+                Remove
+              </button>
+            </div>
+            <div className="span-dash" />
+            <div>Edit</div>
+          </div>
+        </div>
+        <div className="progress">
+          <CircularProgressbar
+            className="circularprogressbar"
+            value={progress}
+          />
+          <div className="progress2">
+            <p>{`${progress}%`}</p>
+            Completed
+          </div>
+        </div>
+        <span className="span" />
+        <div className="chapter">
+          <h4>CURRENT CHAPTER</h4>
+          <span>
+            Chapter
+            {chapter}
+          </span>
+          <button type="button" className="progress-btn">Update Progress</button>
+        </div>
+      </div>
     </div>
   );
 }
